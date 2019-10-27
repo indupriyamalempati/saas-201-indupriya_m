13eb5d838c623976703a321c5186e1c518ab5b7b
indu priya <indupriyamalempati99@gmail.com>
2019-10-27 17:37:49 +0530

    my level-08 code

---
+++ level-08/college_portal/app/models/section.rb
@@ -0,0 +1,9 @@
+class Section < ApplicationRecord
+belongs_to :department
+has_many :students, dependent: :destroy
+validates :name, presence: true, uniqueness: true
+before_save :to_upper
+def to_upper
+    self.name = self.name.upcase
+end
+end
